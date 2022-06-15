0f31c15b7e51f24133c1abc99030f980fc7f51bd
fresvel <fresvel@localhost.localdomain>
2022-06-15 00:56:03 -0400

    Tareas y timer, I2C en cc.c

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/fresvel/esp/Proyectos/ciclismo/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/fresvel/esp/esp-idf/.git")
+set(GIT_DIR "/home/fresvel/esp/Proyectos/ciclismo/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
